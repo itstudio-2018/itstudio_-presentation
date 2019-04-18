2b62a422a978a228087bc3278cdc56d88fee2827
249606097 <249606097@qq.com>
2019-04-18 17:07:48 +0800

    basic admin

--- join/admin.py
+++ join/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Applicant)
+class ApplicantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone_number', 'email', 'year', 'status')
+    list_per_page = 30
+    search_fields = ('name',)
+    list_filter = ('status', 'year')
+    ordering = ('name', 'status')
+
+    def status_1(self, request, queryset):
+        queryset.update(status=1)
+    status_1.short_description = '激活'
+
+    def status_2(self, request, queryset):
+        queryset.update(status=2)
+    status_2.short_description = '通过初审'
+
+    def status_3(self, request, queryset):
+        queryset.update(status=3)
+    status_3.short_description = '通过面试'
+
+    def status_4(self, request, queryset):
+        queryset.update(status=4)
+    status_4.short_description = '选中'
+
+    actions = [status_1, status_2, status_3, status_4]
+
+
+@admin.register(models.Link)
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ('email', 'expiration')
+    list_per_page = 30
+    ordering = ('expiration',)
+    search_fields = ('email',)
--- show/admin.py
+++ show/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'info', 'is_alive')
+    ordering = ('is_alive',)
+    list_filter = ('is_alive',)
+
+
+@admin.register(models.Story)
+class StoryAdmin(admin.ModelAdmin):
+    list_display = ('year', 'title', 'info')
+    list_filter = ('year',)
+    ordering = ('-year',)
+
+
+@admin.register(models.Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year', 'department__name')
+    list_filter = ('year',)
+    search_fields = ('name',)
+    ordering = ('-year',)
+
+
+@admin.register(models.Work)
+class WorkAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+@admin.register(models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('nickname', 'content', 'reply')
+    ordering = ('-time',)
