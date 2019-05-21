e4813dde7a14f7a4ff5d8bd9095b5e36b3284bad
249606097 <249606097@qq.com>
2019-05-21 14:12:08 +0800

    front

--- static/js/animation.js
+++ static/js/animation.js
@@ -6,7 +6,7 @@ $(".join").click(function(){
 //team中左右点击后出现部门介绍和社团介绍，中间圈圈旋转180°
 $(".teamLeft").click(function(){
     $(".Departments").toggle();
-    $(".teamLeft").animate({opacity:'1'})
+    $(".teamLeft").animate({opacity:'1'});
 })
 $(".teamRight").click(function(){
     $(".Definition").toggle();
@@ -22,36 +22,56 @@ $(".teams").click(function(){
     }
 })
 //侧边卡片弹出
-$(".DepartmentsMore").click(function(){
-    $(".DepartmentsDetail").animate({left:'0px'},function(){
-        if($(".DepartmentsDetail").left == 0){
-            
-        }
-        $(document).click(function(e){
-            var target  = $(e.target);
-            if(!target.is('.DepartmentsDetail')){
-                $(".DepartmentsDetail").animate({left:'-2000px'});
-            }
-        })
-    })
+// $(".team").click(function(e){
+//     var target  = $(e.target);
+//     if(!target.is('.DepartmentsDetail')){
+//         $(".DepartmentsDetail").animate({left:'-2000px'});
+//     }
+//     if(target.is('.DepartmentsMore')){
+//         $(".Departmentstail").animate({left:'0px'});
+//     }
+// })
+// $(".team").click(function(e){
+//     var target  = $(e.target);
+//     if(!target.is('.DefinitionDetail')){
+//         $(".DefinitionDetail").animate({left:'-2000px'});
+//     }
+//     if(target.is('.DefinitionMore')){
+//         $(".DefinitionDetail").animate({left:'0px'});
+//     }
+// })
+
+$(".work").click(function(e){
+    var target  = $(e.target);
+    if(!target.is('.worksDetail')){
+        $(".worksDetail").animate({left:'-2000px'});
+    }
+    if(target.is('.moreWorks')){
+        $(".worksDetail").animate({left:'0px'});
+    }
+    if(target.is('.pointer')){
+        $(".worksDetail").animate({left:'0px'});
+    }
 })
-$(".DefinitionMore").click(function(){
-    $(".DefinitionDetail").animate({left:'0px'},function(){
-        $(document).click(function(e){
-            var target  = $(e.target);
-            if(!target.is('.DefinitionDetail')){
-                $(".DefinitionDetail").animate({left:'-2000px'});
-            }
-        })
-    })
+
+//网页作品弹出的卡片点击切换一二页
+$(".pointer1").click(function(){
+    if($(".pointer1").hasClass("pointerColor")){
+    }else{
+        $(".pointer2").removeClass("pointerColor");
+        $(".pointer1").addClass("pointerColor");
+        $(".firstLine1").removeClass("lineChange");
+        $(".secondLine1").removeClass("lineChange");
+        $(".firstLine2").addClass("lineChange");
+    }
 })
-$(".moreWorks").click(function(){
-    $(".worksDetail").animate({left:'0px'},function(){
-        $(document).click(function(e){
-            var target  = $(e.target);
-            if(!target.is('.worksDetail')){
-                $(".worksDetail").animate({left:'-2000px'});
-            }
-        })
-    })
+$(".pointer2").click(function(){
+    if($(".pointer2").hasClass("pointerColor")){
+    }else{
+        $(".pointer1").removeClass("pointerColor");
+        $(".pointer2").addClass("pointerColor");
+        $(".firstLine2").removeClass("lineChange");
+        $(".secondLine1").addClass("lineChange");
+        $(".firstLine1").addClass("lineChange");
+    }
 })
