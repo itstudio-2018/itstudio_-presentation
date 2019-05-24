073189e4c8376a04ea91e291bee2a95a4b6b2b76
249606097 <249606097@qq.com>
2019-05-24 18:09:33 +0800

    front

--- static/js/animation.js
+++ static/js/animation.js
@@ -1,59 +1,38 @@
 //点击加入我们弹出报名表单
-$(".join").click(function(){
-    $(".joinDetail").toggle();
-})
-
-//team中左右点击后出现部门介绍和社团介绍，中间圈圈旋转180°
-$(".teamLeft").click(function(){
-    $(".Departments").toggle();
-    $(".teamLeft").animate({opacity:'1'});
-})
-$(".teamRight").click(function(){
-    $(".Definition").toggle();
-    $(".teamRight").animate({opacity:'1'})
-})
-$(".teams").click(function(){
-    if($(".rolling").hasClass("rotate")){
-        $(".rolling").removeClass("rotate");
-        $(".rolling").addClass("rotate1");
-    }else{
-        $(".rolling").removeClass("rotate1");
-        $(".rolling").addClass("rotate");
-    }
-})
-//侧边卡片弹出
-// $(".team").click(function(e){
-//     var target  = $(e.target);
-//     if(!target.is('.DepartmentsDetail')){
-//         $(".DepartmentsDetail").animate({left:'-2000px'});
-//     }
-//     if(target.is('.DepartmentsMore')){
-//         $(".Departmentstail").animate({left:'0px'});
-//     }
-// })
-// $(".team").click(function(e){
-//     var target  = $(e.target);
-//     if(!target.is('.DefinitionDetail')){
-//         $(".DefinitionDetail").animate({left:'-2000px'});
-//     }
-//     if(target.is('.DefinitionMore')){
-//         $(".DefinitionDetail").animate({left:'0px'});
-//     }
-// })
-
-$(".work").click(function(e){
+$('.nav').click(function(e){
     var target  = $(e.target);
-    if(!target.is('.worksDetail')){
-        $(".worksDetail").animate({left:'-2000px'});
+    if(target.is('.join')){
+        $(".joinDetail").show();
     }
+    if(!target.is('.joinDetail *') && !target.is('.join')){
+        $(".joinDetail").hide();
+    }
+})
+
+
+
+//侧边卡片弹出，隐藏卡片显示(点击了解更多)
+//隐藏部门显示
+$('body').click(function(e){
+    var target  = $(e.target);
+    if(target.is('.moreDepartment')){
+        $(".DepartmentsDetail").show();
+    }
+    if(!target.is('.DepartmentsDetail *') && !target.is('.moreDepartment')){
+        $(".DepartmentsDetail").hide();
+    }
+})
+
+//隐藏的网页作品显示
+$('body').click(function(e){
+    var target  = $(e.target);
     if(target.is('.moreWorks')){
         $(".worksDetail").animate({left:'0px'});
     }
-    if(target.is('.pointer')){
-        $(".worksDetail").animate({left:'0px'});
+    if(!target.is('.worksDetail *') && !target.is('.moreWorks')){
+        $(".worksDetail").animate({left:'-2000px'});
     }
 })
-
 //网页作品弹出的卡片点击切换一二页
 $(".pointer1").click(function(){
     if($(".pointer1").hasClass("pointerColor")){
@@ -73,5 +52,22 @@ $(".pointer2").click(function(){
         $(".firstLine2").removeClass("lineChange");
         $(".secondLine1").addClass("lineChange");
         $(".firstLine1").addClass("lineChange");
+    }
+})
+
+
+
+//member中滑行条拖动（年级条xuanze）
+//使timeCar（滑块）可拖动
+$.Move($('#timeCar'))
+
+
+
+//event模块事件点击后变大
+$(".eventsDetailBox li").click(function(){
+    if($(this).hasClass("eventsBackground")){
+    }else{
+        $(this).siblings().removeClass("eventsBackground");
+        $(this).addClass("eventsBackground");
     }
 })
