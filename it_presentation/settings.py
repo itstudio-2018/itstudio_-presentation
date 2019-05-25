2752984be7d163c609236c3cf939a4faaef14905
server <1@qq.com>
2019-05-25 19:32:11 +0800

    s

--- it_presentation/settings.py
+++ it_presentation/settings.py
@@ -143,9 +143,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
-#CORS_ORIGIN_WHITELIST = (
-#    '*'
-#)
+CORS_ORIGIN_WHITELIST = (
+    '*'
+)
 
 EMAIL_HOST = 'smtp.163.com'
 EMAIL_PORT = 465
