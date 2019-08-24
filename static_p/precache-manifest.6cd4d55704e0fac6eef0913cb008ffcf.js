731a64d836a31b5d785ae937fc2cb42f39df22f6
Leo <249606097@qq.com>
2019-08-24 18:11:58 +0800

    test

---
+++ static_p/precache-manifest.6cd4d55704e0fac6eef0913cb008ffcf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8ec7b1a35019efdb731baa40c744dd6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "809f03d7ee6862df4ede",
+    "url": "./static/css/2.11118f24.chunk.css"
+  },
+  {
+    "revision": "ba170681044817c44a6c",
+    "url": "./static/css/main.8634b187.chunk.css"
+  },
+  {
+    "revision": "809f03d7ee6862df4ede",
+    "url": "./static/js/2.245c0f42.chunk.js"
+  },
+  {
+    "revision": "ba170681044817c44a6c",
+    "url": "./static/js/main.b42f5840.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "be900b675f2939830b65e074cdc8dede",
+    "url": "./static/media/404bg.be900b67.png"
+  },
+  {
+    "revision": "1335002f1b5b62480d865fb91ccfdfa8",
+    "url": "./static/media/SourceHanSansCN-Regular.1335002f.ttf"
+  },
+  {
+    "revision": "1db29588408eadbd4406aae9238555eb",
+    "url": "./static/media/consola.1db29588.ttf"
+  }
+]);
